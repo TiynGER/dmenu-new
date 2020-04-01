3a6a2a8457e4d6690f7baf9543d9acf9970e06a3
TiynGER <kantemarten@googlemail.com>
2020-04-02 01:28:30 +0200

    new color

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a62238", "#000000" },
-	[SchemeSel] = { "#000000", "#a62238" },
+	[SchemeNorm] = { "#bc3c4b", "#000000" },
+	[SchemeSel] = { "#000000", "#bc3c4b" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
